6f771c7ff04f72e1c7262ecf6cb98adecf0e27ce
223880 <rafael.segala45@gmail.com>
2024-08-12 07:28:02 -0300

    Fix

---
+++ rust_l402/src/lib.rs
@@ -0,0 +1,11 @@
+// src/lib.rs or src/main.rs
+mod my_module;
+
+pub fn public_function() {
+    crate::my_module::internal_function();
+}
+
+// src/my_module.rs
+pub(crate) fn internal_function() {
+    println!("This is an internal function.");
+}
