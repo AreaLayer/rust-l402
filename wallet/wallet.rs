7e6c7de06ffd90d90a2b9166c830c424d686c60c
223880 <rafael.segala45@gmail.com>
2024-08-13 06:44:50 -0300

    Add pre image

--- wallet/wallet.rs
+++ wallet/wallet.rs
@@ -2,7 +2,7 @@ pub(crate) fn wallet_function() {
     println!("This is a function in the wallet module.");
 }
 
-use lnd_grpc::lnrpc::{wallet_unlocker_client::WalletUnlockerClient, WalletUnlocker, WalletUnlockerClient};
+use lnd_grpc::lnrpc::{wallet_unlocker_client::WalletUnlockerClient, WalletUnlocker, WalletUnlockerClient, PreImage};
 
 impl WalletUnlockerClient {
     pub async fn unlock_wallet(&mut self, wallet_password: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -13,3 +13,12 @@ impl WalletUnlockerClient {
     Ok(())
     }
 }
+
+impl PreImage {
+    pub fn new(preimage: &[u8]) -> Self {
+        PreImage {
+            preimage: preimage.to_vec(),
+        }
+    }
+}
+
