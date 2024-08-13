1fbdd7cdf8e492e48796922503793ef1eda3f2ae
223880 <rafael.segala45@gmail.com>
2024-08-13 06:59:52 -0300

    Fix pre image

--- wallet/wallet.rs
+++ wallet/wallet.rs
@@ -1,16 +1,22 @@
 pub(crate) fn wallet_function() {
     println!("This is a function in the wallet module.");
+
+    // Example preimage
+    let example_preimage = PreImage::new(b"example_preimage_data");
+
+    // Example use of the preimage
+    println!("Created a new PreImage with data: {:?}", example_preimage.preimage);
 }
 
-use lnd_grpc::lnrpc::{wallet_unlocker_client::WalletUnlockerClient, WalletUnlocker, WalletUnlockerClient, PreImage};
+use lnd_grpc::lnrpc::{wallet_unlocker_client::WalletUnlockerClient, WalletUnlocker, PreImage};
 
 impl WalletUnlockerClient {
     pub async fn unlock_wallet(&mut self, wallet_password: &str) -> Result<(), Box<dyn std::error::Error>> {
         let request = WalletUnlocker::UnlockWalletRequest {
             wallet_password: wallet_password.to_string(),
         };
-    let response = self.unlock_wallet(request).await?;
-    Ok(())
+        let response = self.unlock_wallet(request).await?;
+        Ok(())
     }
 }
 
@@ -21,4 +27,12 @@ impl PreImage {
         }
     }
 }
-
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lnd_grpc::lnrpc::{wallet_unlocker_client::WalletUnlockerClient, WalletUnlocker, PreImage};
+    use tokio;
+    use std::error::Error;
+    use std::time::Duration;
+    use tokio::time::timeout;
+}
