a30217fbc7093fb48dd4963569506a4803e0c1cc
223880 <rafael.segala45@gmail.com>
2024-08-13 10:49:18 -0300

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,39 @@
 mod client {
+    use lnd_grpc_tonic_client::lnrpc::Payment;
+
+    use crate::RustL402;
+
     // use tonic_lnd::Client;
     pub fn client_function() {
         // Implementation goes here
-        loop {
-            // Add your implementation logic here
-            // Break the loop when necessary
-        }
+        let _rust_l402 = RustL402;
+        let _payment = Payment {
+            payment_hash: vec![],
+            value: 0,
+            creation_date: 0,
+            fee: 0,
+            payment_preimage: vec![],
+            value_sat: 0,
+            value_msat: 0,
+            payment_request: String::new(),
+            status: 0,
+            fee_sat: 0,
+            fee_msat: 0,
+            creation_time_ns: 0,
+            htlcs: vec![],
+            payment_index: 0,
+            failure_reason: 0,
+        };
     }
 }
 
 mod wallet {
+    use lnd_grpc_tonic_client::lnrpc::WalletAccountBalance;
+
     pub fn wallet_function() {
-        // Implementation goes here        
+        // Implementation goes here 
+
+        let wallet_account_balance = WalletAccountBalance::default();
     }
 }
 
@@ -20,8 +42,8 @@ mod tokenstore {
 
     pub fn tokenstore_function() {
         // Implementation goes here
-}
-}
+        let memory = Memory::new();
+    }}
 pub struct RustL402;
 
 impl RustL402 {
